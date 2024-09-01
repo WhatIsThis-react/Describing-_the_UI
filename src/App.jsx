faf8ebcdcc48c01fca7efb07bd1b52141426564b
hyorish03 <hyorish2001@gmail.com>
2024-09-02 00:59:06 +0900

    1주차 Starter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        justifyContent: " center",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    ></div>
+  );
+}
+
+export default App;
